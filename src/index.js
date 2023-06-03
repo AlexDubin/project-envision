1f8a837221e371f6f6bda29ebf1d029142d8285f
Oleksandr Dubin <102242242+AlexDubin@users.noreply.github.com>
2023-06-03 15:22:21 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './js/header';
+
+import './js/home_page/section_hero';
+import './js/home_page/weekly_trends';
+import './js/home_page/section_upcoming';
+
+import './js/catalog_page/section_hero';
+import './js/catalog_page/section_catalog';
+
+import './js/libraru_page/section_hero';
+import './js/libraru_page/section_library';
+
+import './js/modals/modal_film';
+import './js/modals/modal_team';
+import './js/modals/modal_trailer';
+
+import './js/footer';
