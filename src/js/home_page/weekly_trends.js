7ac03479be78586c7017fffa53e34608586c6b34
TetianaOgnivenko <tatianaognivenko@gmail.com>
2023-06-05 19:44:59 +0300

    fix bugs genres and stars

--- src/js/home_page/weekly_trends.js
+++ src/js/home_page/weekly_trends.js
@@ -16,7 +16,6 @@ async function getTrendingMoviesByWeek(page = 1) {
       `${BASE_URL}/trending/movie/week?language=en-US&page=${page}`,
       options
     );
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.log(error);
@@ -30,62 +29,63 @@ if (movieList) {
   genres.genres.forEach(genre => {
     genresObject[genre.id] = genre.name;
   });
+  handlerMoviesWeek(genresObject);
+}
 
+function handlerMoviesWeek(genresObject) {
   getTrendingMoviesByWeek()
     .then(data => {
-      console.log(data);
-      markupMovies(data);
+      createMarkupMovies(data, genresObject);
     })
     .catch(err => console.log(err));
+}
+function createMarkupMovies({ results }, genresObject) {
+  const randomIndexes = getRandomMovieToShow(results);
+  // const randomIndexes = [];
+  // while (randomIndexes.length < 3) {
+  //   const randomIndex = Math.floor(Math.random() * results.length);
+  //   if (!randomIndexes.includes(randomIndex)) {
+  //     randomIndexes.push(randomIndex);
+  //   }
+  // }
+  const moviesToShow = randomIndexes.map(index => results[index]);
+  markupMovie(moviesToShow, genresObject);
+}
 
-  function markupMovies({ results }) {
-    console.log({ results });
-    const randomIndexes = [];
-    while (randomIndexes.length < 3) {
-      const randomIndex = Math.floor(Math.random() * results.length);
-      if (!randomIndexes.includes(randomIndex)) {
-        randomIndexes.push(randomIndex);
-      }
-    }
-    const moviesToShow = randomIndexes.map(index => results[index]);
-    movieList.innerHTML = moviesToShow
-      .map(({ poster_path, title, vote_average, genre_ids, release_date }) => {
-        console.log(5, vote_average, release_date);
-        let genreNames = '';
-        if (genre_ids.length > 0) {
-          if (genre_ids.length === 1 || genre_ids.join(', ').length <= 20) {
-            genreNames = genresObject[genre_ids[0]];
-          } else {
-            genreNames = `${genresObject[genre_ids[0]]}, ${
-              genresObject[genre_ids[1]]
-            }`;
-          }
-        }
-        return `<li class='item-movie-card'>
-          <img class='poster-movie-card' src='https://image.tmdb.org/t/p/original${poster_path}' alt='${title}'>
-          <div class='overlay-movie-card'></div>
-          <div class='info-movie-card'>
-            <h4 class='title-movie-card'>${title}</h4>
-            <div class='wrapper-movie-card'>
-              <div class='genre-year-movie-card'>
-                <p class='genre-movie-card span'>${genreNames}</p>
-                <span class='divider-movie-card'>&#124</span>
-                <p class='year-movie-card span'>${release_date.slice(0, 4)}</p>
-              </div>
-              <div class="form-item">
-                <div class="rating">
-                  <div class="rating-body">
-                    <div class="rating-active"></div>
-                    <div class="rating-items">
-                      <input type="radio" class="rating-item" value="1" name="rating" />
-                      <input type="radio" class="rating-item" value="2" name="rating" />
-                      <input type="radio" class="rating-item" value="3" name="rating" />
-                      <input type="radio" class="rating-item" value="4" name="rating" />
-                      <input type="radio" class="rating-item" value="5" name="rating" />
-                    </div>
+function markupMovie(moviesToShow, genresObject) {
+  movieList.innerHTML = moviesToShow
+    .map(({ poster_path, title, vote_average, genre_ids, release_date }) => {
+      const genreNames = getGenres(genresObject, genre_ids);
+
+      return `<li class='item-movie-card'>
+              <img class='poster-movie-card' src='https://image.tmdb.org/t/p/original${poster_path}' alt='${title}'>
+              <div class='overlay-movie-card'></div>
+              <div class='info-movie-card'>
+                <h4 class='title-movie-card'>${title}</h4>
+                <div class='wrapper-movie-card'>
+                  <div class='genre-year-movie-card'>
+                    <p class='genre-movie-card span'>${genreNames}</p>
+                    <span class='divider-movie-card'>&#124</span>
+                    <p class='year-movie-card span'>${release_date.slice(
+                      0,
+                      4
+                    )}</p>
                   </div>
-                <div class="rating-value">${vote_average}</div>
+                    <div class="rating">
+                      <div class="rating-body">
+                        <div class="rating-active"></div>
+                        <div class="rating-items">
+                          <input type="radio" class="rating-item" value="1" name="rating" />
+                          <input type="radio" class="rating-item" value="2" name="rating" />
+                          <input type="radio" class="rating-item" value="3" name="rating" />
+                          <input type="radio" class="rating-item" value="4" name="rating" />
+                          <input type="radio" class="rating-item" value="5" name="rating" />
+                        </div>
+                      </div>
+                    <div class="rating-value">${vote_average}</div>
+                </div>
               </div>
+<<<<<<< Updated upstream
             </div>
           </div>
         </li>`;
@@ -105,16 +105,59 @@ if (movieList) {
     const ratingActive = rating.querySelector('.rating-active');
     const ratingValue = rating.querySelector('.rating-value');
     console.log(ratingValue, ratingActive);
+=======
+            </li>`;
+    })
+    .join('');
+  initRatings();
+}
+>>>>>>> Stashed changes
 
-    if (ratingValue) {
-      setRatingActiveWidth(ratingActive, ratingValue.innerHTML);
+function getGenres(genresObject, genre_ids) {
+  let genreNames = '';
+  const mainGenre = genresObject[genre_ids[0]];
+  const anotherGenre = genresObject[genre_ids[1]];
+
+  if (genre_ids.length > 0) {
+    if (mainGenre.length > 17) {
+      genreNames = mainGenre.split('');
+    }
+    if (mainGenre.length + anotherGenre.length > 17) {
+      genreNames = mainGenre;
+    } else {
+      genreNames = `${mainGenre}, ${anotherGenre}`;
     }
   }
+  return genreNames;
+}
 
-  function setRatingActiveWidth(ratingActive, index) {
-    console.log(4);
-    const ratingActiveWidth = (index / 10) * 100;
-    ratingActive.style.width = `${ratingActiveWidth}%`;
-    console.log(4);
+function initRatings() {
+  const ratings = document.querySelectorAll('.rating');
+  ratings.forEach(rating => {
+    initRating(rating);
+  });
+}
+function initRating(rating) {
+  const ratingActive = rating.querySelector('.rating-active');
+  const ratingValue = rating.querySelector('.rating-value');
+
+  if (ratingValue) {
+    setRatingActiveWidth(ratingActive, ratingValue.innerHTML);
   }
+}
+
+function setRatingActiveWidth(ratingActive, index) {
+  const ratingActiveWidth = (index / 10) * 100;
+  ratingActive.style.width = `${ratingActiveWidth}%`;
+}
+
+function getRandomMovieToShow(results) {
+  const randomIndexes = [];
+  while (randomIndexes.length < 3) {
+    const randomIndex = Math.floor(Math.random() * results.length);
+    if (!randomIndexes.includes(randomIndex)) {
+      randomIndexes.push(randomIndex);
+    }
+  }
+  return randomIndexes;
 }
