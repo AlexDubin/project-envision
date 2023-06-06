05f5d795da75e8db566e2a55f22a9513350945eb
j-Hop <119629763+j-Hop@users.noreply.github.com>
2023-06-07 00:20:47 +0300

    update

--- src/js/header.js
+++ src/js/header.js
@@ -14,4 +14,19 @@
       refs.body.classList.toggle("no-scroll");
     }
   })();
+
+
+  const switchTheme = () => {
+    const rootEl = document.documentElement;
+    let dataTheme = rootEl.getAttribute('data-theme'),
+      newTheme;
+    newTheme = dataTheme === 'light' ? 'dark' : 'light';
   
+    rootEl.setAttribute('data-theme', newTheme);
+  
+    localStorage.setItem('theme', newTheme);
+  };
+
+  document
+  .getElementById('theme-switcher')
+  .addEventListener('click', switchTheme);
