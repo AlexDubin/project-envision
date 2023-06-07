d673b46e3bdfdc1606ccf0cafa3bc5ff764fc217
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-06-07 10:09:01 +0200

    Changed libraryAPI according to amendments

--- src/js/api/libraryAPI.js
+++ src/js/api/libraryAPI.js
@@ -1,6 +1,15 @@
+import axios from 'axios';
+import { Notify } from 'notiflix';
+
 const STORAGE_KEY = 'movies-library';
-const emptyLibEl = document.getElementById('lib-empty');
-const libCatalogEl = document.getElementById('lib-catalog');
+const BASE_URL = 'https://api.themoviedb.org/3';
+const options = {
+  headers: {
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYjE3NzU3ZjU2ZDg1NmM5YzE2MmE1OWEzZWZlMmY5MyIsInN1YiI6IjY0NzhkNWZjOWI2ZTQ3MDBkZThlOTBlZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BE22YVTai34Dh5C1jSxEZ2DzzjnxJfKFgEIpWS2JFcI',
+    accept: 'application/json',
+  },
+};
 
 /*
 Возможности:
@@ -18,10 +27,9 @@ const libCatalogEl = document.getElementById('lib-catalog');
  * @param {Object} movie - movie to save
  * @param {number} movie.id - id of movie (Required)
  * @throws Will throw an error if movie already in library
- * @throws {SyntaxError} Can throw this error cause JSON
  */
 export function saveMovie(movie) {
-  const movies = loadMovies();
+  const movies = loadMoviesFromStorage();
 
   if (movie.id === undefined) throw new Error('No id');
 
@@ -29,42 +37,73 @@ export function saveMovie(movie) {
     throw new Error('Already in library');
 
   movies.push(movie);
-  saveMovies(movies);
+
+  saveMoviesToStorage(movies);
 }
 
 /**
  * Removes movie from localStorage by id
  * @param {number} id - id of movie to delete
- * @throws {SyntaxError} Can throw this error cause JSON
  */
 export function removeMovie(id) {
-  const movieCardToRemove = document.querySelector(`[data-id="${id}"]`);
-  movieCardToRemove?.remove();
+  const movies = loadMoviesFromStorage();
+  const moviesToSave = movies.filter(movie => movie.id !== id);
 
-  let movies = loadMovies();
-
-  movies = movies.filter(movie => movie.id !== id);
-
-  if (movies.length === 0) {
-    emptyLibEl?.classList.remove('is-hidden');
-    libCatalogEl?.classList.add('is-hidden');
-  }
-
-  saveMovies(movies);
+  saveMoviesToStorage(moviesToSave);
 }
 
 /**
  * Loads movies from localStorage
  * @param {number} limit - Limit of returned movies
  * @returns Array of movies
- * @throws {SyntaxError} Can throw this error cause JSON
  */
-export function loadMovies(limit = -1) {
-  const movies = JSON.parse(localStorage.getItem(STORAGE_KEY) ?? '[]');
-  return limit > 0 ? movies.slice(0, limit) : movies;
+export async function loadMovies(limit = 0, page = 1) {
+  try {
+    const previousMovies = page * limit;
+    const savedMovies = loadMoviesFromStorage();
+
+    const moviesToLoad =
+      page <= 1
+        ? savedMovies.slice(0, limit)
+        : savedMovies.slice(previousMovies, previousMovies + limit);
+
+    return await Promise.all(moviesToLoad.map(fetchMovie));
+  } catch (err) {
+    Notify.failure("Can't load movies.");
+    console.error(err);
+    return [];
+  }
 }
 
-function saveMovies(movies) {
-  const moviesString = JSON.stringify(movies);
-  localStorage.setItem(STORAGE_KEY, moviesString);
+function saveMoviesToStorage(movies) {
+  try {
+    const moviesString = JSON.stringify(movies);
+    localStorage.setItem(STORAGE_KEY, moviesString);
+  } catch (err) {
+    Notify.failure("Can't save movies to storage.");
+    console.error(err);
+  }
+}
+
+function loadMoviesFromStorage() {
+  try {
+    return JSON.parse(localStorage.getItem(STORAGE_KEY) ?? '[]');
+  } catch (err) {
+    Notify.failure("Can't load saved movies.");
+    console.error(err);
+    return [];
+  }
+}
+
+async function fetchMovie({ id }) {
+  const response = await axios.get(
+    `${BASE_URL}/movie/${id}?language=en-US`,
+    options
+  );
+  const {
+    data: { poster_path, title, vote_average, release_date, genres },
+  } = response;
+  const genre_ids = genres.map(({ id }) => id);
+
+  return { genre_ids, poster_path, title, vote_average, release_date, id };
 }
