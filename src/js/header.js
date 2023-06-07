e9eac144bfdb22bceb8628113643b2cfd010d692
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-06-07 22:07:04 +0200

    Added mobile menu handler

--- src/js/header.js
+++ src/js/header.js
@@ -1,17 +1,29 @@
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector("[data-menu-open]"),
-      closeMenuBtn: document.querySelector("[data-menu-close]"),
-      menu: document.querySelector("[data-menu]"),
-      body: document.querySelector("body"),
-    };
-  
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
-  
-    function toggleMenu() {
-      refs.menu.classList.toggle("is-hidden");
-      refs.body.classList.toggle("no-scroll");
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+    document.body.classList.toggle('scroll-lock');
+  };
+
+  const checkForBackdropClick = evt => {
+    if (evt.target.classList.contains('js-menu-container')) {
+      toggleMenu();
     }
-  })();
-  
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  mobileMenu.addEventListener('click', checkForBackdropClick);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    document.body.classList.remove('scroll-lock');
+  });
+})();
