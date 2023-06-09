486ef6e1cf8338f191383888dc5bc2dd3f546bd7
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-06-09 12:17:38 +0200

    Added refs for library

---
+++ src/js/refs/library-refs.js
@@ -0,0 +1,7 @@
+export default {
+  genresSelectEl: document.getElementById('genres-select'),
+  moviesListEl: document.getElementById('library-movie-list'),
+  emptyLibEl: document.getElementById('lib-empty'),
+  libCatalogEl: document.getElementById('lib-catalog'),
+  loadMoreBtnEl: document.getElementById('lib-load-more'),
+};
